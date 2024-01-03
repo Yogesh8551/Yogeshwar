35e84d54c18f9672b5c086a8bf243c3be4edd8cd
Yogesh8551 <132248362+Yogesh8551@users.noreply.github.com>
2024-01-04 00:58:14 +0530

    Add files via upload

---
+++ MyResponsive Website/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides:true,
+    navigation: {
+      nextE1: ".swiper-button-next",
+     prevE1: ".swiper-button-prev",
+    },
+  });
